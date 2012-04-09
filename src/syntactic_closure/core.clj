ce9e4423971d187d631b51ad6ce0f697735e3501
OHTA Shogo <athos0220@gmail.com>
2012-04-09 22:34:07 +0900

    first commit

---
+++ src/syntactic_closure/core.clj
@@ -0,0 +1,1 @@
+(ns syntactic-closure.core)
---
+++ test/syntactic_closure/test/core.clj
@@ -0,0 +1,6 @@
+(ns syntactic-closure.test.core
+  (:use [syntactic-closure.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
