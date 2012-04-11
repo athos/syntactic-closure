6f883d85b61900b9579dcb5aafe73dfd3696c6a6
OHTA Shogo <athos0220@gmail.com>
2012-04-12 01:46:08 +0900

    added an example

---
+++ examples/push.clj
@@ -0,0 +1,18 @@
+(ns push
+  (:use [syntactic-closure :only [sc-macro-transformer make-syntactic-closure qq]]))
+
+(defmacro push! [x v]
+  (sc-macro-transformer
+    (fn [env]
+      (let [x' (make-syntactic-closure env nil x)]
+        (qq (set! ~x' (conj ~x' ~(make-syntactic-closure env nil v))))))))
+
+(comment
+  (def ^:dynamic x)
+
+  (binding [x []]
+    (push! x 0)
+    (push! x 1)
+    (push! x 2)
+    x) ;=> [0 1 2]
+  )
