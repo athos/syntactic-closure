4624240b626db40cc99d116608186e8f1c3a2ab6
OHTA Shogo <athos0220@gmail.com>
2012-04-13 01:10:54 +0900

    modified representation of syntactic environment: now it consists of ns-name and locals

--- src/syntactic_closure.clj
+++ src/syntactic_closure.clj
@@ -1,10 +1,15 @@
 (ns syntactic-closure
   (:refer-clojure :exclude [compile])
-  (:use [syntactic-closure.environment :only [filter-environment]]))
+  (:use [syntactic-closure.environment :only [make-environment filter-environment]]))
 
 (load "syntactic_closure/compile")
 (load "syntactic_closure/specials")
 (load "syntactic_closure/quasiquote")
+
+(defmacro define-syntax [name args & body]
+  `(defmacro ~name ~args
+     (let [~'&ns-name '~(ns-name *ns*)]
+       ~@body)))
 
 (defn- syntactic-closur-ize [f]
   (with-meta f {:type ::syntactic-closure}))
@@ -23,10 +28,12 @@
   (syntactic-closur-ize f))
 
 (defmacro sc-macro-transformer [f]
-  (if-let [env (::env (meta &form))]
-    `(make-syntactic-closure {} nil (~f '~env))
-    `(let [env# (reduce (fn [env# [name# val#]]
-                          (assoc env# name# name#))
-                        {}
-                        ~'&env)]
-       (compile env# (make-syntactic-closure {} nil (~f env#))))))
+  `(if-let [env# (::env (meta ~'&form))]
+     (make-syntactic-closure (make-environment ~'&ns-name {}) nil (~f env#))
+     (let [env# (make-environment
+                  (ns-name *ns*)
+                  (into {} (map (fn [[name# val#]] [name# name#]) ~'&env)))]
+       (compile env#
+                (make-syntactic-closure (make-environment ~'&ns-name {})
+                                        nil
+                                        (~f env#))))))
--- src/syntactic_closure/environment.clj
+++ src/syntactic_closure/environment.clj
@@ -3,23 +3,29 @@
 ;;
 ;; syntactic environment
 ;;
-(defn- toplevel-env []
-  (ns-map *ns*))
+(defn make-environment [ns-name locals]
+  {:ns-name ns-name, :locals locals})
+
+(defn- toplevel-env [env]
+  (ns-map (:ns-name env)))
 
 (defn lookup [env name]
-  (or (env name)
-      ((toplevel-env) name)))
+  (or ((:locals env) name)
+      ((toplevel-env env) name)))
 
 (defn add-to-environment [env id alias]
-  (assoc env id alias))
+  (make-environment (:ns-name env)
+                    (assoc (:locals env) id alias)))
 
 (defn extend-environment [env ids]
-  (reduce (fn [env id] (add-to-environment env id (gensym id))) env ids))
+  (make-environment (:ns-name env)
+                    (:locals (reduce #(add-to-environment %1 %2 (gensym %2)) env ids))))
 
 (defn filter-environment [names names-env else-env]
-  (reduce (fn [env [name val]]
-            (if (names name)
-              (assoc env name val)
-              env))
-          else-env
-          names-env))
+  (make-environment (:ns-name else-env)
+                    (:locals (reduce (fn [env name]
+                                       (if-let [var (lookup names-env name)]
+                                         (add-to-environment env name var)
+                                         env))
+                                     else-env
+                                     names))))
