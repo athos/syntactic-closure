6b7c3f79e7796502c13b504e6a24ef168494ed06
OHTA Shogo <athos0220@gmail.com>
2012-05-05 01:04:33 +0900

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject syntactic-closure "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject syntactic-closure "0.1.0"
+  :description "syntactic closures built on top of Clojure's macro system"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :extra-classpath-dirs ["examples"])
